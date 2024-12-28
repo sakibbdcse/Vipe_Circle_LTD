94ba873f67d92a85c7f320f741e155dbe37c7d9c
sakib <sakibbd.cse@gmail.com>
2024-12-28 19:33:05 +0600

    setup front end project

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import RootLayout from './layout/RootLayout';
+import Home from './pages/Home';
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route >
+    )
+  )
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
