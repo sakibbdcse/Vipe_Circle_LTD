392ed48d856cae46c779307d7395ce9ec82a7440
Jafrul Hasan <jafrulhasan.sarkar@gmail.com>
2025-01-31 22:03:44 +0600

    change right menu

--- frontend/src/pages/apps/entertainment/Home.jsx
+++ frontend/src/pages/apps/entertainment/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Grid } from '@mui/material';
+import RightBar from '../../../components/app/entertainment/RightBar';
 const Home = () => {
   return (
-    <div>Home</div>
+    <Grid container spacing={2} sx={{ marginTop: '70px', marginLeft:'70px' }}>
+        <Grid item xs={3}>
+                <div>left</div>
+            </Grid>
+            {/* Middle Section */}
+            <Grid item xs={6}>
+                content
+            </Grid>
+
+            {/* Right Section */}
+            <Grid item xs={3}>
+                <RightBar />
+            </Grid>
+    </Grid>
   )
 }
 
