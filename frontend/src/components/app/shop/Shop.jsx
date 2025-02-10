b3742924b409d8a7a6959064b7f2e26f36c856e1
sakib <sakibbd.cse@gmail.com>
2025-02-10 21:00:02 +0600

    added product photo

--- frontend/src/components/app/shop/Shop.jsx
+++ frontend/src/components/app/shop/Shop.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Container, Grid, Card, CardMedia, CardContent, CardActions, Button } from '@mui/material';
 
 const products = [
-  { id: 1, name: 'Product 1', price: '$20', image: 'https://via.placeholder.com/150' },
-  { id: 2, name: 'Product 2', price: '$30', image: 'https://via.placeholder.com/150' },
-  { id: 3, name: 'Product 3', price: '$40', image: 'https://via.placeholder.com/150' },
-  { id: 4, name: 'Product 4', price: '$50', image: 'https://via.placeholder.com/150' },
+  { id: 1, name: 'Product 1', price: '$20', image: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-madebymath-90946.jpg&fm=jpg' },
+  { id: 2, name: 'Product 2', price: '$30', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9Kos8eEjHDuMCDnm0oJln6u5t-5RPNemHsw&s' },
+  { id: 3, name: 'Product 3', price: '$40', image: 'https://welpix.b-cdn.net/wp-content/uploads/2024/07/The-Importance-of-High-Value-Product-Photography.webp' },
+  { id: 4, name: 'Product 4', price: '$50', image: 'https://welpix.b-cdn.net/wp-content/uploads/2024/07/The-Importance-of-High-Value-Product-Photography.webp' },
 ];
 
 const Shop = () => {
